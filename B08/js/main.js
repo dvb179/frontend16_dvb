50e1e23ac28aa4003e536d669ed56fff16960caf
DVB <dvbinh179@gmail.com>
2022-07-31 14:51:22 +0700

    btt

--- B08/js/main.js
+++ B08/js/main.js
@@ -1,13 +1,15 @@
-const setFunction = () => {
-    let a = 10;
-    let b ='xxx'
-    for (let i = 0; i < a; i++) {
+const myFunction = () => {
+    let a = 9;
+    let b = 'xxx'
+    for (let i = 0; i <= a; i++) {
         let c;
-        c = i + b 
-        for (let j = i + 1; j < a; i++) {
-            c += j + b
+        c = i + b
+
+        for (let j = i + 1; j <= a; j++) {
+            c = c + j + b;
+
         }
+        document.write(c + '</br>')
     }
-    document.write(c)
 }
-setFunction();
+myFunction()
