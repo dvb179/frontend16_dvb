2665a4c63e08d8f1300bece59feba4e6c1fd84c0
DVB <dvbinh179@gmail.com>
2022-07-31 23:23:42 +0700

    bt-

--- B08/js/main.js
+++ B08/js/main.js
@@ -9,7 +9,40 @@ const myFunction = () => {
             c = c + j + b;
 
         }
-        document.write(c + '</br>')
+        console.log(c + '</br>')
     }
 }
 myFunction()
+
+
+/* --------------find max------------------ */
+const findMax = (a, b, c) => {
+    let max = a
+
+    if (b > max) max = b
+    if (c > max) max = c;
+    return max
+}
+
+console.log(findMax(2, 8, 4));
+
+/*--------------find max in array------------------ */
+
+
+const findMaxArray = (NumberList) => {
+    if (!Array.isArray(NumberList) || NumberList.length === 0 || typeof NumberList === '') return -1;
+    //     let max = NumberList[0];
+    //     for (let i = 0; i < NumberList.length; i++) {
+    //         if ((NumberList[i] > 0) && (NumberList[i] > max)) max = NumberList[i];
+
+    //     }
+    //     return max
+    // }
+    // console.log(findMaxArray([23, 43, '6']));
+
+
+
+
+    return NumberList.reduce((max, numbers) => numbers > max ? numbers : max);
+}
+console.log(findMaxArray([23, '43']));
