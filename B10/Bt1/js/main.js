b520cbf2c988b8ae4e31538cbfe5bd0012dfe2d8
DVB <dvbinh179@gmail.com>
2022-08-11 00:18:48 +0700

    bt

--- B10/Bt1/js/main.js
+++ B10/Bt1/js/main.js
@@ -5,33 +5,15 @@ ELENMENT_BUTTON.addEventListener('click', () => {
     toggleForm = !toggleForm
 });
 
-// handle click addTask button
-ELENMENT_INPUT_SUBMIT.addEventListener('click', () => {
-
-// set id to arr
-    let ramdomID = String(Date.now())
-// set name to arr
-    let nameList = ELENMENT_INPUT_NAME.value
-
-    addItemToDOM(ramdomID, nameList)
-    addItemToArr(ramdomID, nameList)
-    ELENMENT_INPUT_NAME.value = '';
-
-});
 
 
-//ELEMENT_FORM_BUTTON.addEventListener('click', function () {
-    handleToggleForm(toggleFrom)
-    toggleFrom = !toggleFrom
-})
-
-ELEMENT_FORM_SUBMIT_BUTTON.addEventListener('click' , function () {
+ELENMENT_INPUT_SUBMIT.addEventListener('click' , function () {
     let id = makeid(15)
-    let name = ELEMENT_FORM_INPUT_NAME.value
-    let level = ELEMENT_FORM_INPUT_LEVEL.value
+    let name = ELENMENT_INPUT_NAME.value
+    let level = ELENMENT_INPUT_LEVEL.value
 
-    arrData.push({id,name,level})
-    console.log(arrData);
-    ShowData()
+    arrList.push({'id':id,'name':name,'level':level})
+    
+    
 })
-
+ShowData()
--- B10/Bt1/js/model.js
+++ B10/Bt1/js/model.js
@@ -9,51 +9,18 @@ const addTask = (open = true) => {
     }
 
 }
-// addTask
-const addItemToDOM = (ramdomID, nameList) => {
-    const tbody = document.querySelector('tbody')
-    
-    const td = document.createElement('td')
-    tbody.setAttribute('data-id', ramdomID)
-    td.innerText = nameList
-    
-    tbody.appendChild(td)
-
-
-}
-
-
-
-const addItemToArr = (ramdomID, nameList) => {
-    arrList.push({ ramdomID, nameList })
-    console.log(arrList);
-}
-console.log(addItemToDOM());
-
-
-const handleToggleForm = (open = true) => {
-    if(open) {
-        ELEMENT_FORM_INPUT.removeAttribute('hidden')
-        ELEMENT_FORM_BUTTON.style.backgroundColor = 'red'
-        ELEMENT_FORM_BUTTON.innerHTML = 'CLOSE TASK'
-    }else {
-        ELEMENT_FORM_INPUT.setAttribute('hidden' , true)
-        ELEMENT_FORM_BUTTON.style.backgroundColor = '#46b8da'
-        ELEMENT_FORM_BUTTON.innerHTML = 'ADD TASK'
-    }
-}
 // Show Data
 const ShowData = () => {
-    let xhtml = '';
-    arrData.forEach((item , index) => {
+    let xhtml= '';
+    arrList.forEach((item , index) => {
         let id = item.id
-        xhtml += ` <tr>
+        xhtml += `<tr>
             <td class="text-center">${index + 1}</td>
             <td>${item.name}</td>
             <td class="text-center"><span class="label label-danger">High</span></td>
             <td>
                 <button type="button" class="btn btn-warning">Edit</button>
-                <button type="button" class="btn btn-danger" onclick="deleteItem('${id}')" >Delete</button>
+                <button type="button" class="btn btn-danger" onclick="deleteItem('${id}')">Delete</button>
             </td>
         </tr>`
     })
@@ -62,7 +29,7 @@ const ShowData = () => {
 
 // delete
 const deleteItem = (id) => {
-    console.log(deleteItem);
+    console.log(id);
 }
 
 // add array
