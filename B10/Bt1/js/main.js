69dbc9336992d7f446bcc1b778c801f8562e7349
ZendVN <dvbinh179@gmail.com>
2022-08-10 21:23:15 +0700

    btt

--- B10/Bt1/js/main.js
+++ B10/Bt1/js/main.js
@@ -18,3 +18,20 @@ ELENMENT_INPUT_SUBMIT.addEventListener('click', () => {
     ELENMENT_INPUT_NAME.value = '';
 
 });
+
+
+//ELEMENT_FORM_BUTTON.addEventListener('click', function () {
+    handleToggleForm(toggleFrom)
+    toggleFrom = !toggleFrom
+})
+
+ELEMENT_FORM_SUBMIT_BUTTON.addEventListener('click' , function () {
+    let id = makeid(15)
+    let name = ELEMENT_FORM_INPUT_NAME.value
+    let level = ELEMENT_FORM_INPUT_LEVEL.value
+
+    arrData.push({id,name,level})
+    console.log(arrData);
+    ShowData()
+})
+
--- B10/Bt1/js/model.js
+++ B10/Bt1/js/model.js
@@ -29,3 +29,56 @@ const addItemToArr = (ramdomID, nameList) => {
     console.log(arrList);
 }
 console.log(addItemToDOM());
+
+
+const handleToggleForm = (open = true) => {
+    if(open) {
+        ELEMENT_FORM_INPUT.removeAttribute('hidden')
+        ELEMENT_FORM_BUTTON.style.backgroundColor = 'red'
+        ELEMENT_FORM_BUTTON.innerHTML = 'CLOSE TASK'
+    }else {
+        ELEMENT_FORM_INPUT.setAttribute('hidden' , true)
+        ELEMENT_FORM_BUTTON.style.backgroundColor = '#46b8da'
+        ELEMENT_FORM_BUTTON.innerHTML = 'ADD TASK'
+    }
+}
+// Show Data
+const ShowData = () => {
+    let xhtml = '';
+    arrData.forEach((item , index) => {
+        let id = item.id
+        xhtml += ` <tr>
+            <td class="text-center">${index + 1}</td>
+            <td>${item.name}</td>
+            <td class="text-center"><span class="label label-danger">High</span></td>
+            <td>
+                <button type="button" class="btn btn-warning">Edit</button>
+                <button type="button" class="btn btn-danger" onclick="deleteItem('${id}')" >Delete</button>
+            </td>
+        </tr>`
+    })
+    ELEMENT_CONTENT.innerHTML = xhtml
+}
+
+// delete
+const deleteItem = (id) => {
+    console.log(deleteItem);
+}
+
+// add array
+
+
+
+//  create ID 
+const makeid = (length) =>  {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+
