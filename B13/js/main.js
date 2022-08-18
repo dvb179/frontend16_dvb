1d34c75edbca4c3d6f943f45ba98f5acf772ab3a
DVB <dvbinh179@gmail.com>
2022-08-18 22:50:27 +0700

    bt

--- B13/js/main.js
+++ B13/js/main.js
@@ -1,21 +1,25 @@
-// let c = true;
-// $("#myImage").click(function () {
-//   let a = $(this).css("background-color", "yellow");
-//   let b = $(this).css("background-color", "aqua");
-//   c ? a : b;
-//   c = !c;
+let c = true;
+let a = $(this).css('background-color', 'red')
+let b = $(this).css('background-color', 'green')
+console.log(a);
+$("#myImage").click(function () {
+    c ? a : b
+  
+     c = !c;
 
-// });
-// let n = 20
-// $('button:first').click(function (e) { 
-//     e.preventDefault();
-//     n++
-//     $('#text').css('font-size', n)
-//     console.log(123);
-// });
+});
 
-$('.box').click(function () { 
-    let color = $(this).css('color');
-    $('#text').css('color', color);
 
+let n = 20
+$('button:first').click(function (e) { 
+    e.preventDefault();
+    n++
+    $('#text').css('font-size', n + 'px')
+    console.log(123);
+});
+
+$('.box').click((e) => { 
+    let colors = $(this).css('background-color');
+    $('#text').css('color', colors);
+    console.log(colors);
 });
