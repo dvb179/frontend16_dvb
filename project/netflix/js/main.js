431a9ebbeb4be8bcb257c6b027ca2be21731d3e0
DVB <dvbinh179@gmail.com>
2022-09-18 22:52:52 +0700

    bt

--- project/netflix/js/main.js
+++ project/netflix/js/main.js
@@ -1,13 +1,26 @@
-BTN_NEXT.addEventListener('click', e => {
+// CLICK BTN
+BTN_NEXT.addEventListener('click', () => {
     handleChangeSlider(1)
     setTimeout(() => {
-        
-    },300)
-    console.log(SLIDER_ITEM_WIDTH);
+
+    }, 300)
 });
 
 
-BTN_PREV.addEventListener('click', e => {
+BTN_PREV.addEventListener('click', () => {
     handleChangeSlider(-1)
     console.log();
 });
+
+// CLICK DOT
+SLIDES_DOT.forEach(item => item.addEventListener('click', (e) => {
+    $('.slider-dot-item.active').classList.remove('active');
+    item.classList.add('active')
+
+    const slideIndex = parseInt(e.target.dataset.index);
+    counter = slideIndex;
+    console.log(slideIndex);
+
+    positionX = -1 * counter * SLIDER_ITEM_WIDTH;
+    SLIDER_MAIN.style = `transform: translateX(${positionX}px)`;
+}));
--- project/netflix/js/modal.js
+++ project/netflix/js/modal.js
@@ -1,28 +1,46 @@
-
-
+// handle Click
 const handleChangeSlider = (dir) => {
     if (dir === 1) {
-        index++;
-        console.log(index);
-        if (index >= SLIDER_ITEM_LENGTH) {
-            positionX = SLIDER_ITEM_WIDTH
-            index = 0
+        console.log(counter);
+        if (counter >= SLIDER_ITEM_LENGTH - 1) {
+            
+            positionX = SLIDER_ITEM_LENGTH - 1
+            counter = -1;
         }
         positionX = positionX - SLIDER_ITEM_WIDTH;
+        console.log(positionX);
         SLIDER_MAIN.style = `transform: translateX(${positionX}px)`
+        counter++;
 
     } else if (dir === -1) {
         console.log('prev')
-        index--;
-        console.log(index);
-        if (index < 0) {
-            index = 0
-            return
+
+        if (counter <= 0) {
+            return counter = 0
         }
         positionX = positionX + SLIDER_ITEM_WIDTH;
         
         SLIDER_MAIN.style = `transform: translateX(${positionX}px)`
+        counter--;
         
-
     }
+    $('.slider-dot-item.active').classList.remove('active');
+    SLIDES_DOT[counter].classList.add('active');
 }
+
+// auto scroll
+// setInterval(() =>{
+//     counter++;
+//     if (counter >= SLIDER_ITEM_LENGTH) {
+//         positionX = SLIDER_ITEM_WIDTH
+//         counter = 0
+//     }
+//     console.log(counter);
+//     positionX = positionX - SLIDER_ITEM_WIDTH;
+//     SLIDER_MAIN.style = `transform: translateX(${positionX}px)`
+    
+//     $('.slider-dot-item.active').classList.remove('active');
+//     SLIDES_DOT[counter].classList.add('active');
+
+// },5000);
+
